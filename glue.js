929fb83781f58295ecd9b7c3b9a4b7ccb71c48d5
Brandon Ros <brandonros1@gmail.com>
2018-06-11 01:34:30 -0400

    4 functions and a multi-component example

---
+++ glue.js
@@ -0,0 +1,40 @@
+var h = function(tagName, properties, children) {
+  var element = document.createElement(tagName);
+
+  Object.keys(properties).forEach(function(property) {
+    element[property] = properties[property];
+  });
+
+  if (Array.isArray(children)) {
+    children.forEach(function(child) {
+      element.appendChild(child);
+    });
+  } else if (typeof children === 'string') {
+    element.appendChild(document.createTextNode(children));
+  }
+
+  return element;
+};
+
+var mergeState = function(oldState, newState) {
+  return Object.assign({}, oldState, newState);
+};
+
+var updateNodes = function(oldNodes, newNodes) {
+  oldNodes.replaceWith(newNodes);
+
+  return newNodes;
+};
+
+var setState = function(key, value) {
+  var self = this;
+
+  return function(event) {
+    var newState = {};
+
+    newState[key] = value;
+
+    self.state = mergeState(self.state, newState);
+    self.nodes = updateNodes(self.nodes, self.render());
+  }
+};
